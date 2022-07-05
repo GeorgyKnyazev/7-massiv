18e8d38dcc93e0108baa04bdf24691a5c83fc4df
Георгий Князев <saint_georgy@yahoo.com>
2022-07-05 20:59:54 +0300

    Добавьте файлы проекта.

---
+++ 7 massiv/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _7_massiv
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string lineWistText = "Дана строка с текстом, используя метод строки String.Split() получить массив слов, которые разделены пробелом в тексте и вывести массив, каждое слово с новой строки";
+            string[] massivOfLineText = lineWistText.Split();
+
+            foreach (string massivLine in massivOfLineText)
+            {
+                Console.WriteLine(massivLine);
+            }
+        }
+    }
+}
